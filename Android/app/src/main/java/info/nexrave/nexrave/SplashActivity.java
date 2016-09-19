8ba9bf440cb06eec627416e439f0937fbc0ce912
Shane Austrie <Shane Austrie>
2016-09-18 22:33:18 -0400

    Added login/register screen, and register screen.

--- Android/app/src/main/java/info/nexrave/nexrave/SplashActivity.java
+++ Android/app/src/main/java/info/nexrave/nexrave/SplashActivity.java
@@ -1,13 +1,16 @@
 package info.nexrave.nexrave;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
